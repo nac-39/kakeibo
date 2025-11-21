37b3a0513f5735f2ba53216409e9ff9b8f0e3b0e
nac-39 <83295760+nac-39@users.noreply.github.com>
2025-11-21 23:39:15 +0900

    feat: db connection sample

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	database "github.com/nac-39/kakeibo/pkg/infrastructure"
+)
+
+func run() error {
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, os.Getenv("DATABASE_URL"))
+	if err != nil {
+		return err
+	}
+	defer conn.Close(ctx)
+
+	// list all authors
+	queries := database.New(conn)
+	authors, err := queries.GetBox(ctx, 0)
+	if err != nil {
+		return err
+	}
+	log.Println(authors)
+	return err
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
